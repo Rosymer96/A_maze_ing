01c8d3f92aeaa143f60e694e37aba586b8617b33
Rosa Vela <rosavela@macbook-pro.local>
2026-06-17 13:07:23 +0200

    Init de maze.generator creado

--- maze/__init__.py
+++ maze/__init__.py
@@ -1,9 +1,10 @@
 from maze.parser.config_parser import parse_config, Config, ConfigError
 from maze.renderer import AsciiRenderer, RenderTheme, run
 from maze.solver import MazeSolver
+from maze.generator import MazeGenerator
 
 __all__ = [
     "parse_config", "Config", "ConfigError",
     "AsciiRenderer", "RenderTheme",
-    "run", "MazeSolver"
+    "run", "MazeSolver", "MazeGenerator"
 ]
---
+++ maze/generator/__init__.py
@@ -0,0 +1,3 @@
+from maze.generator.maze_generator import MazeGenerator
+
+__all__ = ["MazeGenerator"]
