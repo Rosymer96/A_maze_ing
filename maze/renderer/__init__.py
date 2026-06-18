89c0f3f88e84b87e57ecb5efe01cc9aa7550a04e
Rosa Vela <rosymer96@gmail.com>
2026-06-18 16:00:34 +0200

    Programa funcionando, queda reorganizar las clases para que em mazegen sea reusable

--- maze/renderer/__init__.py
+++ maze/renderer/__init__.py
@@ -1,4 +1,4 @@
-from maze.renderer.ascii_render import AsciiRenderer, RenderTheme
-from maze.renderer.display_ui import run, display_maze
+from mazegen.renderer.ascii_render import AsciiRenderer, RenderTheme
+from mazegen.renderer.display_ui import run, display_maze
 
 __all__ = ["AsciiRenderer", "RenderTheme", "run", "display_maze"]
